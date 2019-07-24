ab6a3a6b15b300c2b60582cdb671a7ea16604c2e
Peshotan <peshotanirani@gmail.com>
2019-07-24 15:56:56 -0500

    first commit

---
+++ src/Pallete.js
@@ -0,0 +1,23 @@
+import React , {Component} from 'react';
+
+class Pallete extends Component {
+
+    render(){
+        return (
+            <div className={'Pallete'}>
+
+                {/*Navbar goes here*/}
+
+                <div className={'Pallete__colors'}>
+                    Pallete
+                    {/*Bunch of color boxes*/}
+                </div>
+
+                {/*Footer goes here*/}
+            </div>
+        )
+    }
+
+}
+
+export default Pallete
