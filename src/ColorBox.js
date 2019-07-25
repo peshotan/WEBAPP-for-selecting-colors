d16bf087e5a5aad0b3c1be40bbb9c684a507eb48
Peshotan <peshotanirani@gmail.com>
2019-07-24 22:29:35 -0500

    add copy-overlay animation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,10 +3,43 @@ import './ColorBox.css';
 
 class ColorBox extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            isCoping : false
+        }
+    }
+
+    // handleClick = (e) => {
+    //     this.setState({isCoping : true})
+    //     setTimeout(()=>this.setState({isCoping : false}), 1500)
+    // }
+
+    handleClick = (e) => {
+        this.setState(
+            {isCoping : true},
+            () => setTimeout(()=> {this.setState({isCoping : false})}, 2000)
+        )
+    }
 
     render() {
+
+        let copingOrNot = "";
+
+        (this.state.isCoping) ? copingOrNot = "colorbox__copy-overlay colorbox__copy-overlay__expanded" : copingOrNot = 'colorbox__copy-overlay';
+
         return(
-            <div style={{background : this.props.background.color}} className={'colorbox'}>
+            <div onClick={this.handleClick} style={{background : this.props.background.color}} className={'colorbox'}>
+
+                <div style={{background : this.props.background.color}} className={copingOrNot}>
+                </div>
+
+                <div className={`colorbox__copy-msg ${this.state.isCoping && "show"}`}>
+                    <h1>COPIED!</h1>
+                    <p>{this.props.background.color}</p>
+                </div>
+
+
                 <div className={"colorbox__copy-container"}>
                 <div className={"colorbox__box-content"}>
                     <span>{this.props.background.name}</span>
